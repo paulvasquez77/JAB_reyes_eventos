124ea4f470bff8da3d8e5ab576b4f418fc5c0898
jean.vasquez <poolvasquez.77@gmail.com>
2024-03-11 20:42:52 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,54 @@
-import './App.css'
-import imagen0 from './assets/img/rey_atanagildo.png'
-import imagen1 from './assets/img/rey_sisebuto.png'
-import { useRef } from 'react'
-
+import './App.css';
+import imagen0 from './assets/img/rey_atanagildo.png';
+import imagen1 from './assets/img/rey_sisebuto.png';
+import { useRef } from 'react';
 
 function App() {
-
   const refCaja = useRef();
   const dolar = 4710;
+  const algo = 1;
 
   const incrementar = (e) => {
-    e.target.innerHTML = Number(e.target.innerHTML) + 1
+    e.target.innerHTML = Number(e.target.innerHTML) + 1;
 
     if (e.target.innerHTML >= 10) {
       e.target.innerHTML = 1;
     }
 
     if (e.target.innerHTML >= 8) {
-      e.target.style.backgroundColor = 'yellow'
+      e.target.style.backgroundColor = 'yellow';
     } else {
-      e.target.style.backgroundColor = 'white'
+      e.target.style.backgroundColor = 'white';
     }
-  }
+  };
 
   const convertir = () => {
     refCaja.current.innerHTML = Number(refCaja.current.innerHTML) * dolar;
-  }
+  };
 
   const cambioImg = (j) => {
-    j.target.src.includes('rey_atanagildo') ? j.target.src = imagen1 : j.target.src = imagen0;
-  }
+    j.target.src.includes('rey_atanagildo')
+      ? (j.target.src = imagen1)
+      : (j.target.src = imagen0);
+  };
 
   const lectura = (a) => {
     refCaja.current.innerHTML = a.target.value;
     console.log(a.target.value);
-  }
+  };
 
   return (
     <>
-      <div className='caja' onClick={incrementar} ref={refCaja}>1</div>
+      <div className="caja" onClick={incrementar} ref={refCaja}>
+        1
+      </div>
       <button onClick={convertir}>Aceptar</button>
       <div>
         <img src={imagen0} onClick={cambioImg} />
       </div>
-      <input type="text" className='campo' onChange={lectura} />
-
+      <input type="text" className="campo" onChange={lectura} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
